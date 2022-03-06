74f1903c502de3aab17f181a4744c9721132e001
james-foxcollis-roostify <jfoxcollis@roostify.com>
2022-03-05 19:01:36 -0700

    practice

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,17 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	anInt := 42
+	var p *int = &anInt
+	fmt.Println("Value of p:", *p)
+
+	value1 := 42.3
+	pointer1 := &value1
+	fmt.Println("Value of pointer1:", *pointer1)
+	fmt.Printf("type of pointer1 %T\n", pointer1)
+
+	*pointer1 = *pointer1 / 31
+	fmt.Println("Value of pointer1:", *pointer1)
+	fmt.Println("Value of pointer1:", value1)
+
 }
