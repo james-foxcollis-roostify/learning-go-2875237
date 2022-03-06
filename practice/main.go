e405b4411fd5b276bbaa2026e3a82b05cb95e7a3
james-foxcollis-roostify <jfoxcollis@roostify.com>
2022-03-05 21:19:30 -0700

    practice

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,30 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
+	resp, err := http.Get(url)
+	checkError(err)
+	fmt.Printf("Response type %T\n", resp)
 	fmt.Println("Network requests")
+
+	defer resp.Body.Close()
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+
+	content := string(bytes)
+	fmt.Print(content)
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
