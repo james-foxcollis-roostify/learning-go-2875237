44aff2bf91a3c348e722156f78e0185ffcd70b06
james-foxcollis-roostify <jfoxcollis@roostify.com>
2022-03-05 19:47:30 -0700

    practice

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,27 @@ import (
 
 func main() {
 	fmt.Println("Conditional logic")
+
+	theAnswer := 46
+	var result string
+
+	if theAnswer < 0 {
+		result = "Less than zero"
+	} else if theAnswer == 1 && (theAnswer == 0 || theAnswer > 0) {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+
+	// initialize variable in if statement
+	if x := 42; x < 0 {
+		result = "Less than zero"
+	} else if x == 0 {
+		result = "Equal to zero"
+	} else {
+		result = "Greater than zero"
+	}
+	fmt.Println(result)
+
 }
