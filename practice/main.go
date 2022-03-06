ea15d0c08115873279e6569415b0e193fa5ae6f6
james-foxcollis-roostify <jfoxcollis@roostify.com>
2022-03-05 19:06:02 -0700

    practice

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Blue"
+	colors[2] = "Green"
+	fmt.Println(colors)
+	fmt.Println(colors[0])
 	fmt.Println("Arrays")
+
+	var numbers = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
+	fmt.Println("Number of colors:", len(colors))
+	fmt.Println("Number of numbers:", len(numbers))
 }
