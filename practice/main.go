31c863bac07e412eac0f1f882ee857d69cfd4689
james-foxcollis-roostify <jfoxcollis@roostify.com>
2022-03-05 21:30:50 -0700

    practice

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
@@ -25,5 +27,38 @@ func main() {
 	}
 
 	content := string(bytes)
-	fmt.Print(content)
+	// fmt.Print(content)
+
+	tours := toursFromJson(content)
+
+	for _, tour := range tours {
+		fmt.Println(tour.Name)
+	}
+}
+
+func toursFromJson(content string) []Tour {
+	tours := make([]Tour, 0, 20)
+
+	decoder := json.NewDecoder(strings.NewReader(content))
+	_, err := decoder.Token()
+	checkError(err)
+
+	var tour Tour
+	for decoder.More() {
+		err := decoder.Decode(&tour)
+		checkError(err)
+		tours = append(tours, tour)
+
+	}
+	return tours
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type Tour struct {
+	Name, Price string
 }
