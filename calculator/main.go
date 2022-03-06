f43a1322d5480fe74413d328e26214fee63898b8
james-foxcollis-roostify <jfoxcollis@roostify.com>
2022-03-05 21:00:51 -0700

    Challenge

--- calculator/main.go
+++ calculator/main.go
@@ -10,8 +10,82 @@ import (
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
+	float1 := inputValue()
 
+	float2 := inputValue()
+
+	operator := Operation()
+
+	var result float64
+
+	switch operator {
+	case "+":
+		result = AddValues(float1, float2)
+	case "-":
+		result = SubtractValues(float1, float2)
+	case "*":
+		result = MultiplyValues(float1, float2)
+	case "/":
+		result = DivideValues(float1, float2)
+	}
+
+	fmt.Printf("The result is %v\n\n", result)
+
+}
+
+//Add values
+func AddValues(value1 float64, value2 float64) float64 {
+	sum := value1 + value2
+	return math.Round(sum*100) / 100
+}
+
+//Subtract values
+func SubtractValues(value1 float64, value2 float64) float64 {
+	sub := value1 - value2
+	return math.Round(sub*100) / 100
+}
+
+//Multiply values
+func MultiplyValues(value1 float64, value2 float64) float64 {
+	multi := value1 * value2
+	return math.Round(multi*100) / 100
+}
+
+//Divide values
+func DivideValues(value1 float64, value2 float64) float64 {
+	divided := value1 / value2
+	return math.Round(divided*100) / 100
+}
+
+//Function for input
+// func inputValue() (float64, float64, string) {
+// 	reader := bufio.NewReader(os.Stdin)
+// 	fmt.Print("Value 1: ")
+// 	input1, _ := reader.ReadString('\n')
+// 	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		panic("Value 1 must be a number")
+// 	}
+
+// 	fmt.Print("Value 2: ")
+// 	input2, _ := reader.ReadString('\n')
+// 	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		panic("Value 2 must be a number")
+// 	}
+
+// 	fmt.Print("Select an Operation (+ - * /)")
+// 	input3, _ := reader.ReadString('\n')
+// 	operator := strings.TrimSpace(input3)
+
+// 	return float1, float2, operator
+// }
+
+//Refactored function for input
+func inputValue() float64 {
+	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Value 1: ")
 	input1, _ := reader.ReadString('\n')
 	float1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
@@ -20,16 +94,15 @@ func main() {
 		panic("Value 1 must be a number")
 	}
 
-	fmt.Print("Value 2: ")
-	input2, _ := reader.ReadString('\n')
-	float2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
-	if err != nil {
-		fmt.Println(err)
-		panic("Value 2 must be a number")
-	}
+	return float1
+}
 
-	sum := float1 + float2
-	sum = math.Round(sum*100) / 100
-	fmt.Printf("The sum of %v and %v is %v\n\n", float1, float2, sum)
+//Refactored to separate operator into function
+func Operation() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Select an Operation (+ - * /)")
+	input, _ := reader.ReadString('\n')
+	operator := strings.TrimSpace(input)
 
+	return operator
 }
